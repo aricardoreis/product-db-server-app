14f6131bd79e6577e7d6bafd93efda2ec63128aa
André Reis <arrr.cin@gmail.com>
2022-08-22 21:58:39 -0400

    Update firestore.ts

--- src/db/firestore.ts
+++ src/db/firestore.ts
@@ -16,9 +16,7 @@ export const getInstanceDB = async () => {
     initializeApp({
       credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY
-          ? Buffer.from(process.env.FIREBASE_PRIVATE_KEY).toString()
-          : undefined,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       }),
     });
