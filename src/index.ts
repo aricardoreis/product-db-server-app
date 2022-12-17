7bb8187337185887adc25fc9b8365411dbc8c1d1
André Reis <arrr.cin@gmail.com>
2022-12-16 22:32:56 -0400

    Change server running port

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { ApplicationError } from "./utils/exception";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
